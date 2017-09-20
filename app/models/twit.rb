82c47fcc2348ce8fb508c4df2c1f41492e517e4d
bernardocaputo <bernardocaputo@gmail.com>
2017-09-20 01:37:17 -0300

    rollback to factory girl adjust branch

--- app/models/twit.rb
+++
@@ -1,8 +0,0 @@
-class Twit < ApplicationRecord
-  belongs_to :user
-  default_scope -> { order(created_at: :desc) }
-  enum status: [:public, :private]
-  validates :user_id, presence: true
-  validates :title, presence: true, length: { maximum: 15 }
-  validates :content, presence: true, length: { maximum: 140 }
-end
