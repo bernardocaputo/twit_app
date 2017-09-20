38c8ec764dcf015bdd32c00026cd1c2ff5c9db4b
bernardocaputo <bernardocaputo@gmail.com>
2017-09-19 21:47:06 -0300

    limiting ffaker sentences

--- spec/factories/twit.rb
+++ spec/factories/twit.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
  factory :twit do
-   title         { FFaker::Lorem.word }
-   content   { FFaker::Lorem.sentence }
+   title         { FFaker::Lorem.words[1..5] }
+   content   { FFaker::Tweet.tweet[1..140] }
    status { Twit.statuses.values.sample }
    user
  end
