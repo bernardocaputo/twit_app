3230882c6876cbc29b3289da67764c9de3aad25a
bernardocaputo <bernardocaputo@gmail.com>
2017-09-20 22:36:21 -0300

    changing name to username on devise and adjusting factory

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20170919195641) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string   "username",                            null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
--- spec/factories/twit.rb
+++ spec/factories/twit.rb
@@ -1,8 +1,6 @@
 FactoryGirl.define do
  factory :twit do
-   title         { FFaker::Lorem.words[1..5] }
    content   { FFaker::Tweet.tweet[1..140] }
-   status { Twit.statuses.values.sample }
    user
  end
 end
