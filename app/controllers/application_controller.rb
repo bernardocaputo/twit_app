22ed0d1802cbcaebaf16b72bce32410d2954147a
bernardocaputo <bernardocaputo@gmail.com>
2017-09-28 01:08:53 -0300

    setup redirect after successful sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < ActionController::Base
   def render_404
     redirect_to main_app.root_url
   end
+
+  protected
+
+    def after_sign_in_path_for(resource)
+      request.env['omniauth.origin'] || stored_location_for(resource) || twits_path
+    end
 end
